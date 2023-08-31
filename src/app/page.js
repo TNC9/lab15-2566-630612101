bda4caecd33e146748380223e59b454c6292c46b
TNC9 <koonbenzkub@hotmail.com>
2023-08-31 15:55:47 +0700

    Finish

--- src/app/page.js
+++ src/app/page.js
@@ -44,7 +44,10 @@ const schema = z
     }),
     hasCoupon: z.boolean(),
     coupon: z.string(),
-    password: z.string(),
+    password: z
+      .string()
+      .min(6, { message: "Password must contain at least 6 characters" })
+      .max(12, { message: "Password must not exceed 12 characters" }),
     confirmPassword: z.string(),
   })
   .refine(
@@ -63,8 +66,17 @@ const schema = z
       message: "Invalid coupon code",
       path: ["coupon"],
     }
+  )
+  .refine(
+    (data) => {
+      if (data.password === data.confirmPassword) return true;
+      return false;
+    },
+    {
+      message: "Password does not match",
+      path: ["confirmPassword"],
+    }
   );
-
 export default function Home() {
   const [opened, { open, close }] = useDisclosure(false);
 
@@ -91,11 +103,14 @@ export default function Home() {
 
     if (form.values.plan === "funrun") price = 500;
     //check the rest plans by yourself
+    if (form.values.plan === "mini") price = 800;
+    if (form.values.plan === "half") price = 1200;
+    if (form.values.plan === "full") price = 1500;
     //TIP : check /src/app/libs/runningPlans.js
-
     //check discount here
+    if (form.values.hasCoupon && form.values.coupon === "CMU2023")
+      return (price -= price * 0.3);
 
-    return price;
   };
 
   return (
@@ -175,7 +190,11 @@ export default function Home() {
           </Stack>
         </form>
 
-        <Footer year={2023} fullName="Chayanin Suatap" studentId="650610560" />
+        <Footer
+          year={2023}
+          fullName="Thananchai Chaimanee"
+          studentId="630612101"
+        />
       </Container>
 
       <TermsAndCondsModal opened={opened} close={close} />
